c31921ab7d630006cd0113b4468a23fc317374fd
EsterlinROCANDY <153329864+EsterlinROCANDY@users.noreply.github.com>
2024-11-22 22:37:26 -0500

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,9 @@
+document.getElementById('recharge-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  alert('Recharge effectuée avec succès ! Merci de votre confiance.');
+});
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  alert('Merci pour votre message ! Nous vous répondrons rapidement.');
+});
